445e9f215ecf1ce7c5731226cbe457b163247c06
Jenny <163994398+0-jenny@users.noreply.github.com>
2024-06-10 17:56:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1008 ms, Memory: 93596 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,49 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int order = 1;
+    private ArrayList<ArrayList<Integer>> graph;
+    private int[] visited;
+    private int order;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int r = sc.nextInt();
-
-        // 인접 리스트 초기화
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+    public Main(int numOfNodes) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= numOfNodes; i++) {
             graph.add(new ArrayList<>());
         }
+        visited = new int[numOfNodes + 1];
+        order = 1;
+    }
 
-        int[] visited = new int[n + 1];
+    public void addEdge(int u, int v) {
+        graph.get(u).add(v);
+        graph.get(v).add(u);
+    }
 
-        for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        // 각 리스트를 오름차순으로 정렬
-        for (int i = 1; i <= n; i++) {
+    public void sortGraph() {
+        for (int i = 1; i < graph.size(); i++) {
             Collections.sort(graph.get(i));
-        }
-
-        dfs(graph, visited, r);
-
-        for (int i = 1; i <= n; i++) {
-            System.out.println(visited[i]);
         }
     }
 
-    private static void dfs(ArrayList<ArrayList<Integer>> graph, int[] visited, int start) {
+    public void dfs(int start) {
         visited[start] = order++;
         for (int neighbor : graph.get(start)) {
             if (visited[neighbor] == 0) {
-                dfs(graph, visited, neighbor);
+                dfs(neighbor);
             }
         }
+    }
+
+    public void printVisitedOrder() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 노드의 수
+        int m = Integer.parseInt(st.nextToken()); // 간선의 수
+        int r = Integer.parseInt(st.nextToken()); // 시작 노드
+
+        Main dfs = new Main(n);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            dfs.addEdge(u, v);
+        }
+
+        dfs.sortGraph();
+        dfs.dfs(r);
+        dfs.printVisitedOrder();
     }
 }
