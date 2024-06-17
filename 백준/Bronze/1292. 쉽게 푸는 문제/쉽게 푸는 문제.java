561126dd25921a8fc5a01e6316c3fc124bcd5449
Jenny <163994398+0-jenny@users.noreply.github.com>
2024-06-17 16:06:58 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -1,34 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
 
-        // 구간의 합을 저장할 변수를 초기화
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int sum = 0;
+        int count = 0;
 
-        // 수열을 저장할 ArrayList를 생성
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
-        // 수열 생성
-        int i = 1;
-        while (i <= 1000) {  // 1부터 1000까지 반복합
-            for (int j = 1; j <= i; j++) {  // i를 i번 리스트에 추가
-                list.add(i);
+        for (int i = 1; count < b; i++) {
+            for (int j = 0; j < i; j++) {
+                count++;
+                if (count >= a && count <= b) {
+                    sum += i;
+                }
+                if (count == b) {
+                    break;
+                }
             }
-            i++;
         }
-
-        // A부터 B까지의 합을 계산합
-        for (int k = A - 1; k < B; k++) {  // 리스트의 인덱스는 0부터 시작하므로 A-1부터 시작
-            sum += list.get(k);  // 해당 구간의 값을 sum에 더하기
-        }
-
-
         System.out.println(sum);
     }
 }
