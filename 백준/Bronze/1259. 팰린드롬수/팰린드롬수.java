3ce5c103874cf77e694dd7c212b6a0e299fda2d0
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-08-01 21:26:35 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            System.out.println(input.equals(new StringBuilder(input).reverse().toString()) ? "yes" : "no");
+        }
+
+
+    }
+}
