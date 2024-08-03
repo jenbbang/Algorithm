56c9c1936d7b18396774361cacb8501ff116f807
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-08-03 21:06:47 +0900

    [Silver V] Title: 영화감독 숌, Time: 260 ms, Memory: 85740 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int num = 666;
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+
+
+    }
+}
