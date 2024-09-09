fce8a60519ded38f3df6e3fa65ba5df9dcf15bef
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-09-09 21:16:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // 테스트 케이스의 수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
 
         // 최대 n이 10이므로, 11까지의 dp 배열을 미리 계산
-        int[] dp = new int[11];
+       int dp[] = new int[11];
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-        // dp 배열을 채움
         for (int i = 4; i <= 10; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-
-        // 각 테스트 케이스에 대해 결과 출력
-        for (int t = 0; t < T; t++) {
-            int n = sc.nextInt();
-            System.out.println(dp[n]);
+        for (int i = 0; i < t; i++) {
+         int num = Integer.parseInt(br.readLine());
+            System.out.println(dp[num]);
         }
     }
 }
