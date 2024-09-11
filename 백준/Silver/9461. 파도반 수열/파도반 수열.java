bbbcc8c0bcc8136f0c816b425df9d7256312a307
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-09-11 21:37:53 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); 
+
+        // 미리 큰 배열을 만들어서 파도반 수열 값 저장
+        long[] P = new long[101];
+        P[1] = 1;
+        P[2] = 1;
+        P[3] = 1;
+        P[4] = 2;
+        P[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            P[i] = P[i-1] + P[i-5];
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(P[N]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
