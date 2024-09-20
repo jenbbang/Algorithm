324da35ec5e9ffdb65b1a5b9dd7a9ad2dcf1881c
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-09-20 21:17:59 +0900

    [Silver II] Title: 최소 힙, Time: 856 ms, Memory: 39836 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,27 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
-
+import java.util.StringTokenizer;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        int N = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(sc.nextLine());
-            if (x == 0) {
-                if (minHeap.isEmpty()) {
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 0) {
+                if (pq.isEmpty()) { // 우선순위 큐가 비어있으면 0을 출력
                     System.out.println(0);
-                } else {
-                    System.out.println(minHeap.poll());
+                } else { // 우선순위 큐가 비어있지 않다면, 최소값(우선순위 큐의 첫 번째 요소)을 출력하고 제거
+                    System.out.println(pq.poll());
                 }
-            } else {
-                minHeap.add(x);
+            } else { // 입력 값이 0이 아니라면, 우선순위 큐에 해당 숫자를 추가
+                pq.add(num);
             }
+
         }
-        sc.close();
+
     }
 }
