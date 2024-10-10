e0d29053d5d6a765110346ef5b0d6e07dc3067fd
Jenny <163994398+jenbbang@users.noreply.github.com>
2024-10-10 21:55:47 +0900

    [Silver I] Title: 경로 찾기, Time: 148 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 정점의 수
+        int[][] graph = new int[n][n];
+
+        // 그래프 초기화
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        // 플로이드-워셜 알고리즘
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1; // i에서 j로 가는 경로가 존재
+                    }
+                }
+            }
+        }
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(graph[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
